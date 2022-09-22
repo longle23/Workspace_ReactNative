be3a2ea796eb074f807ffb0d00491b551db9f02a
longle23 <91379062+longle23@users.noreply.github.com>
2022-09-22 10:15:40 +0700

    Commit TH4

--- TH3/Lab/App.js
+++ TH3/Lab/App.js
@@ -1,124 +1,125 @@
-import React from 'react';
-import {
-  View,
-  Text,
-  ImageBackground,
-  Image,
-  TextInput,
-  TouchableOpacity,
-} from 'react-native';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, Image, Button, TouchableOpacity } from 'react-native';
 
-function Bai1c(props) {
+export default function App() {
   return (
-    <ImageBackground source={require('./img/background.png')} style={{ flex: 100 }}>
-      <View style={{ flex: 15, justifyContent: 'center' }}>
-        <Text
-          style={{
-            fontSize: 50,
-            fontWeight: 'bold',
-            alignSelf: 'center',
-          }}>
-          CODE
-        </Text>
+    <View style={styles.container}>
+      <View style={styles.header}>
 
-        <Text
-          style={{
-            fontSize: 20,
-            marginTop: 20,
-            fontWeight: 'bold',
-            alignSelf: 'center',
-          }}>
-          VERIFICATION
-        </Text>
-      </View>
+        <View style={styles.header1}>
+          <Image source={require('./img/sachgiaokhoa.png')}></Image>
 
-      <View style={{ flex: 15, justifyContent: 'center' }}>
-        <Text
-          style={{
-            textAlign: 'center',
-            fontWeight: 'bold',
-            alignSelf: 'center',
-          }}>
-          Enter ontime password sent on ++849092605798
-        </Text>
+          <Text>Nguyên hàm tích phân và ứng dụng</Text>
 
-        <View
-          style={{
-            flexDirection: 'row',
-            paddingVertical: 5,
-            marginVertical: 5,
-            justifyContent: 'center',
-          }}>
-          <TouchableOpacity
-            style={{
-              height: 40,
-              width: 40,
-              borderColor: 'black',
-              borderWidth: 1,
-            }} />
+          <Text>cung cấp bởi Tiki Trading</Text>
 
-          <TouchableOpacity
-            style={{
-              height: 40,
-              width: 40,
-              borderColor: 'black',
-              borderWidth: 1,
-            }} />
+          <Text style={styles.styleText}>141.800 đ</Text>
+        </View>
 
-          <TouchableOpacity
-            style={{
-              height: 40,
-              width: 40,
-              borderColor: 'black',
-              borderWidth: 1,
-            }} />
+        <View styles={styles.header2}>
+          <Text>Mã giảm giá đã lưu</Text>
 
-          <TouchableOpacity
-            style={{
-              height: 40,
-              width: 40,
-              borderColor: 'black',
-              borderWidth: 1,
-            }} />
+          <TouchableOpacity>
+            <Text>Xem tại đây</Text>
+          </TouchableOpacity>
+        </View>
 
-          <TouchableOpacity
-            style={{
-              height: 40,
-              width: 40,
-              borderColor: 'black',
-              borderWidth: 1,
-            }} />
+        <View style={styles.header3}>
+          <Text>Mã giảm giá</Text>
 
-          <TouchableOpacity
-            style={{
-              height: 40,
-              width: 40,
-              borderColor: 'black',
-              borderWidth: 1,
-            }} />
+          <button value={Áp u}}></button>
         </View>
       </View>
 
-      <View style={{ flex: 15, alignItems: 'center' }}>
-        <TouchableOpacity
-          style={{
-            height: 40,
-            width: 350,
-            justifyContent: 'center',
-            backgroundColor: '#E3C000',
-          }}>
+      <View style={styles.center}>
+        <Text style={styles.textBold}>Bạn có phiếu quà tặng Tiki/Got it/Urbox?</Text>
 
-          <Text
-            style={{
-              alignItems: 'center',
-              textAlign: 'center',
-              fontWeight: 'bold',
-            }}>
-            VERIFY CODE
-          </Text>
+        <Text style={{ color: 'blue' }}>Nhập tại đây?</Text>
+      </View>
+
+      <View style={styles.bottom}>
+        <View style={styles.tamtinh}>
+          <Text style={{ marginRight: 220, fontWeight: 'bold' }}>Tạm tính</Text>
+
+          <Text style={styles.styleText}>141.800 đ</Text>
+        </View>
+
+        <View style={styles.nen}>
+          <Text></Text>
+        </View>
+
+        <View style={styles.thanhtien}>
+          <Text style={{ marginRight: 220, fontWeight: 'bold' }}>Thành tiền </Text>
+
+          <Text style={styles.styleText}>141.800 đ</Text>
+        </View>
+
+        <TouchableOpacity>
+          <Text>Tiến hành đặt hàng</Text>
         </TouchableOpacity>
       </View>
-    </ImageBackground>
+
+      <StatusBar style="auto" />
+    </View>
   );
 }
-export default Bai1c;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#BCB7B6',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  header: {
+    flex: 5,
+    backgroundColor: '#fff',
+    width: '100%',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  center: {
+    flex: 1,
+    backgroundColor: '#fff',
+    marginVertical: 20,
+    width: '100%',
+    justifyContent: 'center',
+    flexDirection: 'row',
+    alignItems: 'center'
+  },
+  bottom: {
+    flex: 5,
+    backgroundColor: '#fff',
+    width: '100%',
+    alignItems: 'center',
+    // justifyContent: 'center',
+  },
+  tamtinh: {
+    flex: 1,
+    justifyContent: 'center',
+    backgroundColor: '#fff',
+    width: '100%',
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  nen: {
+    flex: 2,
+    backgroundColor: '#BCB7B6',
+    width: '100%'
+  },
+  thanhtien: {
+    flex: 1,
+    backgroundColor: '#fff',
+    width: '100%',
+    justifyContent: 'center',
+    alignItems: 'center',
+    flexDirection: 'row',
+    display: 'flex'
+  },
+  styleText: {
+    color: 'red'
+  },
+  textBold: {
+    fontWeight: 'bold',
+  }
+});
