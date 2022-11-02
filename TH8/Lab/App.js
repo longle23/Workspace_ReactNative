54768b622e6b274e1abca378cc819ca0b3a462a6
longle23 <longle2305@gmail.com>
2022-11-02 23:13:32 +0700

    Lab 6

--- TH8/Lab/App.js
+++ TH8/Lab/App.js
@@ -17,11 +17,11 @@ export default function App() {
 
       {/* <Lab3/> */}
 
-      <Lab4/>
+      {/* <Lab4/> */}
 
       {/* <Lab5/> */}
 
-      {/* <Lab6/> */}
+      <Lab6/>
 
       {/* <Lab7/> */}
     </View>
--- TH8/Lab/src/screen/Lab6.js
+++ TH8/Lab/src/screen/Lab6.js
@@ -1,14 +1,71 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { Animated, Button, Image, StyleSheet, Text, View } from "react-native";
+import React, { useEffect, useRef } from "react";
+import meme from '../../assets/meme.png'
 
-const Lab6 = () => {
+const Cau6 = () => {
+
+    const rotate = useRef(new Animated.Value(0)).current;
+
+    useEffect(() => {
+        Animated.loop(
+            Animated.sequence(
+                // lac qua phai 45def
+                [
+                    Animated.timing(rotate, {
+                        toValue: 1,
+                        duration: 1000,
+                        useNativeDriver: false,
+                    }),
+
+                    // lac qua trai 90 deg
+                    Animated.timing(rotate, {
+                        toValue: -1,
+                        duration: 1000,
+                        useNativeDriver: false,
+                    }),
+
+                    // lac qua phai 45 deg: 0 ,90 def: 1
+                    Animated.timing(rotate, {
+                        toValue: 0,
+                        duration: 1000,
+                        useNativeDriver: false,
+                    }),
+                ]
+            )
+        ).start();
+
+    }, []);
+
+    const spin = rotate.interpolate({
+        inputRange: [-1, 0, 1],
+        outputRange: ["-45deg", "0deg", "45deg"],
+    });
     return (
-        <View>
-            <Text>Lab6</Text>
+        <View style={styles.container}>
+            <Animated.View
+                style={[
+                    styles.transition,
+                    {
+                        transform: [{ rotate: spin }],
+                    },
+                ]}
+            >
+                <Image
+                    source={meme}
+                    style={{
+                        width: 100,
+                        height: 200,
+                    }}
+                />
+            </Animated.View>
         </View>
-    )
-}
+    );
+};
 
-export default Lab6
+export default Cau6;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    transition: {
+        marginBottom: 300,
+    },
+});
